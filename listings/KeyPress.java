f7375ed1ed29a3f73fe472cdb9e0dfecc7f6d827
septsea <2997324423@qq.com>
2022-06-03 07:00:16 +0800

    update on Dragon Boat Festival

--- listings/KeyPress.java
+++ listings/KeyPress.java
@@ -1,31 +1,27 @@
-import java.awt.AWTException;
-import java.awt.Robot;
-import java.awt.event.KeyEvent;
+import java.awt.AWTException                                ;
+import java.awt.Robot                                       ;
+import java.awt.event.KeyEvent                              ;
 
-public class KeyPress {
+public class KeyPress                                       {
 
-    public static Robot robot;
+    public static Robot robot                               ;
 
-    static {
-        try {
-            robot = new Robot();
-        } catch (AWTException e) {
-            e.printStackTrace();
-        }
-    }
+    static                                                  {
+        try                                                 {
+            robot = new Robot()                             ;}
+        catch (AWTException e)                              {
+            e.printStackTrace()                             ;}}
 
-    public static void main(String[] args) {
+    public static void main(String... args)                 {
         // There are 82 pages.
-        int n = 41;
-        System.out.println("Begin.");
-        robot.delay(20000);
-        for (int i = 0; i < n; i -= -1) {
-            robot.keyPress(KeyEvent.VK_RIGHT);
-            robot.keyRelease(KeyEvent.VK_RIGHT);
-            System.out.println(i);
-            robot.delay(2000);
-        }
-        System.out.println("End.");
-    }
-
-}
+        int n = 41                                          ;
+        System.out.println("Begin.")                        ;
+        robot.delay(20000)                                  ;
+        int i = 0                                           ;
+        while (i < n)                                       {
+            robot.keyPress(KeyEvent.VK_RIGHT)               ;
+            robot.keyRelease(KeyEvent.VK_RIGHT)             ;
+            System.out.println(i)                           ;
+            robot.delay(2000)                               ;
+            i -=- 1                                         ;}
+        System.out.println("End.")                          ;}}
